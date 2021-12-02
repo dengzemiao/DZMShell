3ffcbd29fae6319154e6ea2930d30f91a260d37c
dengzemiao <dengzemiaovip@163.com>
2021-12-02 14:16:58 +0800

    2021-12-02 14:16:58 提交优化

--- gitpush.sh
+++ gitpush.sh
@@ -14,13 +14,19 @@ cb=$(git branch | sed -n '/\* /s///p')
 # Git
 git add .
 git commit -m "$msg"
+echo "234234234-------"
 # 拉取冲突
 cpmsg=$(git pull origin $cb)
 if [[ $cpmsg =~ "冲突" || $cpmsg =~ "CONFLICT" ]]
 then
   echo "\033[1;31m============================== 合并冲突 pull $cb ==============================\033[0m"
 fi
-echo $cpmsg
+# 拉取失败
+if [[ $cpmsg =~ "fatal" || $cpmsg =~ "ERROR" ]]
+then
+  echo "\033[1;31m============================== 拉取失败 pull $cb ==============================\033[0m"
+fi
+echo "234234234-------"$cpmsg
 git push origin $cb
 
 # 结束
